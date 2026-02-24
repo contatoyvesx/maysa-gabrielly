fae19517f44faf49b1f1dfd4f86a2e495dcf775d
contatoyvesx <contato.yvesx@gmail.com>
2026-02-24 17:18:26 -0300

    Ajusta altura do banner no mobile

--- src/components/easter/Hero.tsx
+++ src/components/easter/Hero.tsx
@@ -4,7 +4,7 @@ import heroImage from "@/assets/hero-easter.jpg";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-[85vh] flex items-center justify-center overflow-hidden pt-16">
+    <section className="relative min-h-[65vh] sm:min-h-[85vh] flex items-center justify-center overflow-hidden pt-16">
       {/* Background image */}
       <div className="absolute inset-0">
         <img
